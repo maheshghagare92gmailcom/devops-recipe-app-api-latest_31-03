947aa95a5d96b795a64984a486945b97c2b21da3
mahesh ghagare <maheshghagare92@gmail.com>
2025-04-04 17:56:40 +0530

    added setup terra

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket               = "devops-recipe-app-tf-state-2702"
+    bucket               = "devops-recipe-app-tf-state-1249"
     key                  = "tf-state-deploy"
     workspace_key_prefix = "tf-state-deploy-env"
     region               = "us-east-1"
