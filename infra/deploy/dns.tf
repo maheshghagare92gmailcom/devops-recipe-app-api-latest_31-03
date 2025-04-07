4408dc653c9e70cd72e0885bc57226dc4cf992c3
mahesh ghagare <maheshghagare92@gmail.com>
2025-04-07 09:59:23 +0530

    added ecs new new new new serives ecs new iam new dns new new new new new

--- infra/deploy/dns.tf
+++ infra/deploy/dns.tf
@@ -3,7 +3,7 @@ resource "aws_route53_zone" "zone" {
 }
 
 resource "aws_route53_record" "app" {
-  zone_id = data.aws_route53_zone.zone.zone_id
+  zone_id = aws_route53_zone.zone.zone_id
   name    = "${lookup(var.subdomain, terraform.workspace)}.${aws_route53_zone.zone.name}"
   type    = "CNAME"
   ttl     = "300"
@@ -34,7 +34,7 @@ resource "aws_route53_record" "cert_validation" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.zone.zone_id
+  zone_id         = aws_route53_zone.zone.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
