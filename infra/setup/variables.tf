516570e5d9991d08aa25f70da41ad9ea8e32cfdc
mahesh ghagare <maheshghagare92@gmail.com>
2025-04-08 10:50:58 +0530

    added ecs new new new new serives ecs new iam new dns new new new new new

--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -3,15 +3,7 @@ variable "prefix" {
   default     = "raa"
 }
 
-variable "project" {
-  description = "Project name for tagging resources"
-  default     = "recipe-app-api"
-}
 
-variable "contact" {
-  description = "Contact email for tagging resources"
-  default     = "mark@example.com"
-}
 
 variable "db_username" {
   description = "Username for the recipe app api database"
