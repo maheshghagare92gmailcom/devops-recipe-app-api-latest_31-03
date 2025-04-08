60bc450327af229a37303960bdf5b3b12533d463
mahesh ghagare <maheshghagare92@gmail.com>
2025-04-08 10:48:26 +0530

    added ecs new new new new serives ecs new iam new dns new new new new new

--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -1,11 +1,6 @@
-variable "tf_state_bucket" {
-  description = "Name of S3 bucket in AWS for storing TF state"
-  default     = "devops-recipe-app-tf-state-1249"
-}
-
-variable "tf_state_lock_table" {
-  description = "Name of DynamoDB table for TF state locking"
-  default     = "devops-recipe-app-api-tf-lock"
+variable "prefix" {
+  description = "Prefix for resources in AWS"
+  default     = "raa"
 }
 
 variable "project" {
@@ -14,7 +9,43 @@ variable "project" {
 }
 
 variable "contact" {
-  description = "Contact name for tagging resources"
+  description = "Contact email for tagging resources"
   default     = "mark@example.com"
 }
 
+variable "db_username" {
+  description = "Username for the recipe app api database"
+  default     = "recipeapp"
+}
+
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
+
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repo with the proxy image"
+}
+
+variable "ecr_app_image" {
+  description = "Path to the ECR repo with the API image"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django"
+}
+
+variable "dns_zone_name" {
+  description = "Domain name"
+  default     = "mahesh1249.store"
+}
+
+variable "subdomain" {
+  description = "Subdomain for each environment"
+  type        = map(string)
+
+  default = {
+    prod    = "api"
+    staging = "api.staging"
+    dev     = "api.dev"
+  }
+}
