d5fc1d2a4dea8d89ee14bcb3e3e45f1700e146ea
mahesh ghagare <maheshghagare92@gmail.com>
2025-04-08 23:50:17 +0530

    added ecs new new new new serives ecs new iam new dns new new new new new new test 6

--- infra/deploy/ecs_new.tf
+++ infra/deploy/ecs_new.tf
@@ -1,40 +1,49 @@
 ##
 # ECS Cluster for running app on Fargate.
 ##
+
 resource "aws_iam_policy" "task_execution_role_policy" {
   name        = "${local.prefix}-task-exec-role-policy"
   path        = "/"
   description = "Allow ECS to retrieve images and add to logs."
   policy      = file("./templates/ecs/task-execution-role-policy.json")
 }
+
 resource "aws_iam_role" "task_execution_role" {
   name               = "${local.prefix}-task-execution-role"
   assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
 }
+
 resource "aws_iam_role_policy_attachment" "task_execution_role" {
   role       = aws_iam_role.task_execution_role.name
   policy_arn = aws_iam_policy.task_execution_role_policy.arn
 }
+
 resource "aws_iam_role" "app_task" {
   name               = "${local.prefix}-app-task"
   assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
 }
+
 resource "aws_iam_policy" "task_ssm_policy" {
   name        = "${local.prefix}-task-ssm-role-policy"
   path        = "/"
   description = "Policy to allow System Manager to execute in container"
   policy      = file("./templates/ecs/task-ssm-policy.json")
 }
+
 resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
   role       = aws_iam_role.app_task.name
   policy_arn = aws_iam_policy.task_ssm_policy.arn
 }
+
 resource "aws_cloudwatch_log_group" "ecs_task_logs" {
   name = "${local.prefix}-api"
 }
+
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
+
 resource "aws_ecs_task_definition" "api" {
   family                   = "${local.prefix}-api"
   requires_compatibilities = ["FARGATE"]
@@ -43,6 +52,7 @@ resource "aws_ecs_task_definition" "api" {
   memory                   = 512
   execution_role_arn       = aws_iam_role.task_execution_role.arn
   task_role_arn            = aws_iam_role.app_task.arn
+  
   container_definitions = jsonencode(
     [
       {
@@ -160,10 +170,12 @@ resource "aws_security_group" "ecs_service" {
   }
   # HTTP inbound access
   ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 8000
+    to_port   = 8000
+    protocol  = "tcp"
+    security_groups = [
+      aws_security_group.lb.id
+    ]
   }
 }
 
@@ -177,13 +189,19 @@ resource "aws_ecs_service" "api" {
   enable_execute_command = true
 
   network_configuration {
-    assign_public_ip = true
+   
 
     subnets = [
-      aws_subnet.public_a.id,
-      aws_subnet.public_b.id
+      aws_subnet.private_a.id,
+      aws_subnet.private_b.id
     ]
 
     security_groups = [aws_security_group.ecs_service.id]
+  }
+  
+   load_balancer {
+    target_group_arn = aws_lb_target_group.api.arn
+    container_name   = "proxy"
+    container_port   = 8000
   }
 }
