5a1637268039964049612d4abaf36acceaacb779
mahesh ghagare <maheshghagare92@gmail.com>
2025-04-09 00:17:17 +0530

    added ecs new new new new serives ecs new iam new dns new new new new new new test 12

--- infra/deploy/dns.tf
+++ infra/deploy/dns.tf
@@ -1,70 +1,42 @@
-#################
-# Load Balancer #
-#################
-resource "aws_security_group" "lb" {
-  description = "Configure access for the Application Load Balancer"
-  name        = "${local.prefix}-alb-access"
-  vpc_id      = aws_vpc.main.id
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    protocol    = "tcp"
-    from_port   = 8000
-    to_port     = 8000
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_route53_zone" "zone" {
+  name = "${var.dns_zone_name}."
 }
-resource "aws_lb" "api" {
-  name               = "${local.prefix}-lb"
-  load_balancer_type = "application"
-  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-  security_groups    = [aws_security_group.lb.id]
-}
-resource "aws_lb_target_group" "api" {
-  name        = "${local.prefix}-api"
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"
-  port        = 8000
-  health_check {
-    path = "/api/health-check/"
-  }
-}
-resource "aws_lb_listener" "api" {
-  load_balancer_arn = aws_lb.api.arn
-  port              = 80
-  protocol          = "HTTP"
+resource "aws_route53_record" "app" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${lookup(var.subdomain, terraform.workspace)}.${data.aws_route53_zone.zone.name}"
+  type    = "CNAME"
+  ttl     = "300"
 
-  default_action {
-    type = "redirect"
+  records = [aws_lb.api.dns_name]
+}
 
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
+resource "aws_acm_certificate" "cert" {
+  domain_name       = aws_route53_record.app.name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
     }
   }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.zone.zone_id
 }
 
-resource "aws_lb_listener" "api_https" {
-  load_balancer_arn = aws_lb.api.arn
-  port              = 443
-  protocol          = "HTTPS"
-
-  certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.api.arn
-  }
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
