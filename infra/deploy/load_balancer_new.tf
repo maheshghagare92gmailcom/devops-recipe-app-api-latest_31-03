8171be060d94744a5af0db23228dd0e9ca338394
mahesh ghagare <maheshghagare92@gmail.com>
2025-04-15 12:24:48 +0530

    remove certificate new

--- infra/deploy/load_balancer_new.tf
+++ infra/deploy/load_balancer_new.tf
@@ -1,22 +1,26 @@
 #################
 # Load Balancer #
 #################
+
 resource "aws_security_group" "lb" {
   description = "Configure access for the Application Load Balancer"
   name        = "${local.prefix}-alb-access"
   vpc_id      = aws_vpc.main.id
+
   ingress {
     protocol    = "tcp"
     from_port   = 80
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     protocol    = "tcp"
     from_port   = 8000
@@ -24,12 +28,14 @@ resource "aws_security_group" "lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_lb" "api" {
   name               = "${local.prefix}-lb"
   load_balancer_type = "application"
   subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
   security_groups    = [aws_security_group.lb.id]
 }
+
 resource "aws_lb_target_group" "api" {
   name        = "${local.prefix}-api"
   protocol    = "HTTP"
